1e0e676dd37fa31c811d1edd123ab51855db2b74
Ian Czekala <iancze@gmail.com>
2020-09-19 17:18:44 -0400

    refactored to constants.py

--- src/casascripts/channels.py
+++ src/casascripts/channels.py
@@ -1,4 +1,9 @@
-def LSRKvel_to_chan(msfile, field, obsid, spw, restfreq, LSRKvelocity):
+import table
+
+from .constants import cc
+
+
+def vel_to_chan(msfile, field, obsid, spw, restfreq, vel):
     """
     Identifies the channel(s) corresponding to input LSRK velocities. 
     Useful for choosing which channels to split out or flag if a line is 
@@ -6,12 +11,12 @@ def LSRKvel_to_chan(msfile, field, obsid, spw, restfreq, LSRKvelocity):
 
     Args:
         msfile (string): name of measurement set 
-        field (str): field name
+        field (string): field name
         spw (int): Spectral window number 
         obsid (int): Observation ID corresponding to the selected 
             spectral window 
         restfreq (float): Rest frequency [Hz]
-        LSRKvelocity (float or array of floats): input velocity in LSRK frame 
+        vel (float or array of floats): input velocity in LSRK frame 
             km/s]
             
 
@@ -19,36 +24,16 @@ def LSRKvel_to_chan(msfile, field, obsid, spw, restfreq, LSRKvelocity):
         (array) or (int) channel number most closely corresponding to 
             input LSRK velocity 
     """
-    cc = 299792458.0  # speed of light in m/s
 
-    print("In LSRKvel", field, spw, restfreq, LSRKvelocity)
-
-    # IC: the problem we have here is that for the second execution,
-    # "DATA_DESC_ID" does not map to the same spectral windows that `listobs`
-    # tells us about it seems like these things are only used to find obsid,
-    # so maybe we can find it a different way
-    # just specifying it works fine
-
-    # tb.open(msfile)
-    # spw_col = tb.getcol('DATA_DESC_ID')
-    # obs_col = tb.getcol('OBSERVATION_ID')
-    # print("spw_col", spw_col)
-    # print("spw_col_unique", np.unique(spw_col))
-    # print("obs_col", obs_col)
-
-    # tb.close()
-    # obsid = np.unique(obs_col[np.where(spw_col==spw)])
-    # print("obsid", obsid)
-
-    tb.open(msfile + "/SPECTRAL_WINDOW")
-    chanfreqs = tb.getcol("CHAN_FREQ", startrow=spw, nrow=1)
-    tb.close()
-    tb.open(msfile + "/FIELD")
-    fieldnames = tb.getcol("NAME")
-    tb.close()
-    tb.open(msfile + "/OBSERVATION")
-    obstime = np.squeeze(tb.getcol("TIME_RANGE", startrow=obsid, nrow=1))[0]
-    tb.close()
+    table.open(msfile + "/SPECTRAL_WINDOW")
+    chanfreqs = table.getcol("CHAN_FREQ", startrow=spw, nrow=1)
+    table.close()
+    table.open(msfile + "/FIELD")
+    fieldnames = table.getcol("NAME")
+    table.close()
+    table.open(msfile + "/OBSERVATION")
+    obstime = np.squeeze(table.getcol("TIME_RANGE", startrow=obsid, nrow=1))[0]
+    table.close()
     nchan = len(chanfreqs)
     ms.open(msfile)
     lsrkfreqs = ms.cvelfreqs(
@@ -60,16 +45,16 @@ def LSRKvel_to_chan(msfile, field, obsid, spw, restfreq, LSRKvelocity):
         start=0,
         outframe="LSRK",
     )
-    # converted to LSRK velocities in km/s
-    chanvelocities = (restfreq - lsrkfreqs) / restfreq * cc / 1.0e3
+    # convert to LSRK velocities [km/s]
+    chanvelocities = (restfreq - lsrkfreqs) / restfreq * cc_kms
     ms.close()
-    if type(LSRKvelocity) == np.ndarray:
-        outchans = np.zeros_like(LSRKvelocity)
-        for i in range(len(LSRKvelocity)):
-            outchans[i] = np.argmin(np.abs(chanvelocities - LSRKvelocity[i]))
+    if type(vel) == np.ndarray:
+        outchans = np.zeros_like(vel)
+        for i in range(len(vel)):
+            outchans[i] = np.argmin(np.abs(chanvelocities - vel[i]))
         return outchans
     else:
-        return np.argmin(np.abs(chanvelocities - LSRKvelocity))
+        return np.argmin(np.abs(chanvelocities - vel))
 
 
 def get_flagchannels(ms_dict, output_prefix, velocity_range=np.array([-20, 20])):
@@ -89,7 +74,7 @@ def get_flagchannels(ms_dict, output_prefix, velocity_range=np.array([-20, 20]))
     """
     flagchannels_string = ""
     for j, spw in enumerate(ms_dict["line_spws"]):
-        chans = LSRKvel_to_chan(
+        chans = vel_to_chan(
             ms_dict["vis"],
             ms_dict["field"],
             ms_dict["obsids"][j],
---
+++ src/casascripts/constants.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+cc_kms = 2.99792458e5  # speed of light in km/s
